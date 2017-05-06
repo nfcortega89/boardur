83557471884a9cd3e6b62152ccb568decb2f83df
Nikko Ortega <NFCOrtega89@gmail.com>
2017-05-05 21:42:06 -0700

    env config variables

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
+const {DATABASE_URL_TEST} = require('../server/config')
 
 before((done) => {
-  mongoose.connect(process.env.DATABASE_URL_TEST || 'mongodb://localhost/trendr-test')
+  mongoose.connect(DATABASE_URL_TEST)
   mongoose.connection
     .once('open', () => { done() })
     .on('error', (error) => {
