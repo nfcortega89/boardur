400688a9bc4507cce1fdc777eaa45200cfde44b4
Nikko Ortega <NFCOrtega89@gmail.com>
2017-08-01 18:22:51 -0700

    crawler

--- server/utils/fetchInstagram.js
+++ server/utils/fetchInstagram.js
@@ -1,63 +1,85 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
-const Horseman = require('node-horseman')
-const config = {
-  userAgent: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0",
-  headers: {
-    "Host": "www.instagram.com",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-    // "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive",
-    "Upgrade-Insecure-Requests": "1"
-  }
-}
+const Nightmare = require('nightmare');
+
 const fetchInstagram = function(instagramId) {
-  const horseman = new Horseman()
-  if (!instagramId) {
-    return Promise.reject( new Error('no instagramId') )
-  }
-  console.log(instagramId)
   return new Promise((resolve, reject) => {
-    try {
-      horseman
-        .userAgent(config.userAgent)
-        .headers(config.headers)
-        .viewport(1440, 2500)
-        .open(`https://www.instagram.com/${instagramId}`)
-        .click('a:contains("Load")')
-        .scrollTo(99999, 0)
-        .wait(1000)
-        .scrollTo(0, 0)
-        .wait(1000)
-        .scrollTo(99999, 0)
-        .wait(1000)
-        .scrollTo(0, 0)
-        .wait(1000)
-        .scrollTo(99999, 0)
-        .wait(1000)
-        .scrollTo(0, 0)
-        .wait(1000)
-        .scrollTo(99999, 0)
-        .wait(1000)
-        .html()
-        .then(function(body) {
-          const $ = cheerio.load(body)
-          let images = []
-          $('img').each(function(i) {
-            // console.log($(this))
-            let obj = {
-              url: $(this).attr('src'),
-              title: $(this).attr('alt') // fetch image title using jquery
-            }
-             images.push(obj)
-          })
-          resolve(images)
-        })
-        .close()
-    } catch(e) {
-      fetchInstagram(instagramId)
-    }
+    Nightmare({ show: true })
+      .goto(`https://www.instagram.com/${instagramId}`)
+      .evaluate(() => document.querySelectorAll('a')[12].click())
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .scrollTo(999999, 0)
+      .wait(500)
+      .evaluate(function () {
+        let images = []
+        document.querySelectorAll('img').forEach(image => images.push({
+          url: image.src,
+          title: image.alt
+        }))
+        return images
+      })
+      .end()
+      .then(function (result) {
+        console.log('fetched', result.length, 'images')
+        resolve(result)
+      })
+      .catch(function (error) {
+        console.error('Search failed:', error)
+        reject(error)
+      })
   })
 }
 
